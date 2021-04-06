36372f31fb90f119931d6773f202f3909cfc87d2
joshua Korf <joshkorf08@gmail.com>
2021-04-06 19:09:01 -0400

    exercises.js update

---
+++ Develop/models/exercises.js
@@ -0,0 +1,11 @@
+const mogoose = require("mongoose");
+const schema = mongoose.schema;
+const exerciseSchema = new schema({
+    name: { type: String, required: true },
+    sets: { type: Number },
+    reps: { type: Number },
+    weight: { type: Number },
+    minutes: { type: Number },
+});
+const Exercises = mongoose.model("Exercises", exerciseSchema);
+module.exports = Exercises;
