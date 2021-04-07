7183753e9fa22db9307ae9e12afba6fe38bb6f56
joshua Korf <joshkorf08@gmail.com>
2021-04-06 20:31:08 -0400

    started work on server.js

---
+++ Develop/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const db = require("./models");
+
+const apiRoutes = require(".routes/api");
+
+const htmlRoutes = require("./routes/html");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { 
+    
+    useNewUrlParser: true 
+});
+
+app.listen(PORT, () => {
+  console.log("listening on port: ", PORT);
+});
