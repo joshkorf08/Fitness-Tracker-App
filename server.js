15c500523eebedf2d8b722b69a0235a705ab7ae5
joshua Korf <joshkorf08@gmail.com>
2021-04-07 20:13:30 -0400

    fixed my local hosting issuegit status

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
+require("dotenv").config()
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || `"mongodb+srv://joshuakorf:${process.env.PASSWORD}@cluster0.pabj8.mongodb.net/test"`, { 
+console.log(`"mongodb+srv://joshuakorf:${process.env.PASSWORD}@cluster0.pabj8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"`)
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://joshuakorf:${process.env.PASSWORD}@cluster0.pabj8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, { 
   
     useNewUrlParser: true, 
     useFindAndModify: false
